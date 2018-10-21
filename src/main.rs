fa9a81462f76af545f9a451b273d6faa4b59a23f
Swoorup Joshi <swoorupjoshi@gmail.com>
2018-10-21 10:32:53 +0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+extern crate structopt;
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+use offregisters_lib::run_from_path;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// Activate debug mode
+    #[structopt(short = "d", long = "debug")]
+    debug: bool,
+
+    /// Config file
+    #[structopt(short = "c", long = "config", parse(from_os_str))]
+    config: PathBuf,
+}
+
+fn main() -> Result<(), io::Error> {
+    let options = Opt::from_args();
+
+    println!("Running from config...");
+    let result = run_from_path(options.config);
+
+    println!("Finished...");
+
+    result
 }
